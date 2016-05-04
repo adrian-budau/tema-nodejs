e48833c8ac24973f44edee5722feaee2e999504a
Adrian Budau <budau.adi@gmail.com>
2016-05-04 14:11:50 +0300

    Changed application name (also first commit).

--- modules/core/client/app/config.js
+++ modules/core/client/app/config.js
@@ -1,7 +1,7 @@
 (function (window) {
   'use strict';
 
-  var applicationModuleName = 'mean';
+  var applicationModuleName = 'poker';
 
   var service = {
     applicationModuleName: applicationModuleName,
