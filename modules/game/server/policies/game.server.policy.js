685b4e11ce5840f11ac88e0fbeec3d9cd0a4c54c
Adrian Budau <budau.adi@gmail.com>
2016-05-06 23:26:18 +0300

    Added server policy for game api

---
+++ modules/game/server/policies/game.server.policy.js
@@ -0,0 +1,74 @@
+'use strict';
+
+/**
+ * Module dependencies
+ */
+var acl = require('acl');
+
+// Using the memory backend
+acl = new acl(new acl.memoryBackend());
+
+/**
+ * Invoke Game Permissions
+ */
+exports.invokeRolesPolicies = function () {
+  acl.allow([{
+    roles: ['admin'],
+    allows: [{
+      resources: '/api/game',
+      permissions: '*'
+    }, {
+      resources: '/api/game/id/:gameId',
+      permissions: '*'
+    }, {
+      resources: '/api/game/me',
+      permissions: '*'
+    }]
+  }, {
+    roles: ['user'],
+    allows: [{
+      resources: '/api/game',
+      permissions: ['get']
+    }, {
+      resources: '/api/game/id/:gameId',
+      permissions: ['get', 'post', 'put']
+    }, {
+      resources: '/api/game/me',
+      permissions: ['get']
+    }]
+  }]);
+};
+
+/**
+ * Check If Game Policy Allows
+ */
+exports.isAllowed = function (req, res, next) {
+  var roles = (req.user) ? req.user.roles : ['guest'];
+
+  // If an article is being processed and the current user created it then allow any manipulation
+  if (req.article && req.user && req.article.user && req.article.user.id === req.user.id) {
+    return next();
+  }
+
+  // Check for user roles
+  acl.areAnyRolesAllowed(roles, req.route.path, req.method.toLowerCase(), function (err, isAllowed) {
+    if (err) {
+      // An authorization error occurred
+      return res.status(500).send('Unexpected authorization error');
+    } else {
+      if (isAllowed) {
+        // Access granted! Invoke next middleware
+        return next();
+      } else {
+        return res.status(403).json({
+          message: 'User is not authorized'
+        });
+      }
+    }
+  });
+};
+
+exports.playAllowed = function(user) {
+  var roles = user ? user.roles : ['guest'];
+  return roles.indexOf('user') !== -1 || roles.indexOf('admin') !== -1;
+};
